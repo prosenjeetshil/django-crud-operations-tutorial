345cce95bc0a063dc53ce33a0c8d65ae215675cc
prosenjeetshil <prosenjeetshil@gmail.com>
2023-09-13 17:30:49 +0530

    feat: model form class

---
+++ crudproject/crudapp/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Orders
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        models = Orders
+        fields = '__all__'
+
+    labels = {
+        'oid': 'Order ID',
+        'fname' : 'First Name',
+        'lname' : 'Last Name.' ,
+        'price' : 'Price' ,
+        'mail' : 'Email ID',
+        'addr' : 'Address' ,
+    }
+
+    widgets  ={
+        'oid' : forms.NumberInput(attrs={'placeholder': 'eg. 101'}),
+        'fname' : forms.TextInput(attrs={'placeholder': 'eg. Prosenjeet'}),
+        'lname' : forms.TextInput(attrs={'placeholder': 'eg. Shil'}),
+        'price' : forms.NumberInput(attrs={'placeholder': 'eg. 10000'}),
+        'mail' : forms.EmailInput(attrs={'placeholder': 'eg. abc@xyz.com'}),
+        'addr' : forms.Textarea(attrs={'placeholder': 'eg. IN'}),
+    }
