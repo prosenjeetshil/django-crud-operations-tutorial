14d83d6368705d0cacd4ad77d3c786a66f5a1386
prosenjeetshil <prosenjeetshil@gmail.com>
2023-09-13 17:16:52 +0530

    feat: models class

--- crudproject/crudapp/models.py
+++ crudproject/crudapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Orders(models.Model):
+    oid = models.IntegerField(primary_key=True)
+    fname = models.CharField(max_length=20)
+    lname = models.CharField(max_length=20)
+    price = models.FloatField()
+    mail = models.EmailField()
+    addr = models.CharField(max_length=50)
