05e03a5b84b164c19904a70b4b51a9ae732f2473
prosenjeetshil <prosenjeetshil@gmail.com>
2023-09-13 17:32:28 +0530

    feat: created template structure

--- crudproject/crudproject/settings.py
+++ crudproject/crudproject/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'crudproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
